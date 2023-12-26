59b56db3843c5f3d972d234578f26ba5c609d963
Alexander <alexander.advanced@gmail.com>
2023-12-26 12:17:46 +0100

    update

--- src/Helper/StringHelper.php
+++ src/Helper/StringHelper.php
@@ -7,9 +7,9 @@ class StringHelper
     static public function toReadable($string): string
     {
         // remove everything, except letters, numbers and spaces
-        $string = preg_replace('/[^a-zA-Z\d\s]/', ' ', $string);
+        $string = preg_replace('/[^\p{L}\p{N}\s]+/u', '', $string);
         // 'SuperEXTRAString' -> 'Super EXTRA String'
-        $string = preg_replace('/[A-Z]([A-Z](?![a-z]))*/', ' $0', $string);
+        $string = mb_ereg_replace('/(?<!^)(\p{Lu})/u', ' $1', $string);
         // remove multiply spaces and trim
         $string = trim(preg_replace('/\s{1,}/', ' ', $string));
         // lowercase everything, uppercase only first letter
